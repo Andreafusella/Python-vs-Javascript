c2fb9fe364d4105a0099c928a40b7a03d2700afc
Riccardo Giangiulio <riccardogiangi@gmail.com>
2024-04-04 12:24:49 +0200

    modifiche 2

--- python.py
+++ python.py
@@ -4,28 +4,36 @@ lista0_100000 = []
 lista0_1000 = []
 lista0_mil = []
 
-def posizionamento(scelta):
-    tempo_inizio = time.perf_counter()
-    if scelta == 1:
-        n = 1000
-    elif scelta == 2:
-        n = 100000
-    elif scelta == 3: 
-        n = 10000000
-    for i in range (n):
+
+def posizionamneto(n):
+    somma = 0
+    for i in range(n):
         k = random.randrange(0,1001)
         if n == 1000:
+            tempo_inizio = time.perf_counter()
             lista0_1000.append(k)
+            tempo_fine = time.perf_counter()
+            tempo_totale = tempo_fine - tempo_inizio
+            somma += tempo_totale
         elif n == 100000:
+            tempo_inizio = time.perf_counter()
             lista0_100000.append(k)
-        elif n == 1000000:
+            tempo_fine = time.perf_counter()
+            tempo_totale = tempo_fine - tempo_inizio
+            somma += tempo_totale
+        elif n == 10000000:
+            tempo_inizio = time.perf_counter()
             lista0_mil.append(k)
-    tempo_fine = time.perf_counter()
-    tempo_totale = tempo_fine - tempo_inizio
-    print(f"{tempo_totale:.2f}")
-        
-scelta = int(input("Inserisci il valore: 1 | 1000 numeri, 2 | 100.000 numeri, 3 | 1.000.000 numeri, 4 | tutti"))
-posizionamento(scelta)
+            tempo_fine = time.perf_counter()
+            tempo_totale = tempo_fine - tempo_inizio
+            somma += tempo_totale
+    print(f'{somma * 10:.3f}')
+    
+
+posizionamneto(1000)
+posizionamneto(100000)
+posizionamneto(10000000)
+
 
 
 
